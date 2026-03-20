3e27fa5b67173f0b21c4f36402148587f00ccd25
Hoang Ky <vanghoangky2511@gmail.com>
2026-03-20 12:28:09 +0700

    email

--- src/main/java/web/expense_management/services/EmailService.java
+++ src/main/java/web/expense_management/services/EmailService.java
@@ -1,6 +1,7 @@
 package web.expense_management.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,15 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    // Tự động lấy email mà bạn đã cài trên Render (biến EMAIL_USER)
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
     public void sendEmail(String to, String subject, String body) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
-            // Bạn nhớ thay email của bạn vào dòng dưới này nhé
-            message.setFrom("ExpensePro <email_cua_ban@gmail.com>"); 
+            // Cấu hình người gửi tự động
+            message.setFrom("ExpensePro <" + senderEmail + ">"); 
             message.setTo(to);
             message.setSubject(subject);
             message.setText(body);
@@ -23,7 +28,12 @@ public class EmailService {
             mailSender.send(message);
             System.out.println("Đã gửi email thành công tới: " + to);
         } catch (Exception e) {
+            // In thẳng lỗi ra Logs của Render để dễ bắt bệnh nếu gửi hỏng
+            e.printStackTrace();
             System.out.println("Lỗi khi gửi email: " + e.getMessage());
+            
+            // Ném lỗi ra ngoài để AuthController biết mà báo về Frontend
+            throw new RuntimeException("Không thể gửi email OTP: " + e.getMessage());
         }
     }
 }
